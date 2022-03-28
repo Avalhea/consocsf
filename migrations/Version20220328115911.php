2df30cf94a8ab0182da483a63d936e05ba8e0df0
Avalhea <emmaperez44@outlook.fr>
2022-03-28 14:34:26 +0200

    roles (en outre)

---
+++ migrations/Version20220328115911.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220328115911 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, echelle_id INT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), INDEX IDX_8D93D649DD268C11 (echelle_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D649DD268C11 FOREIGN KEY (echelle_id) REFERENCES echelle (id)');
+        $this->addSql('DROP TABLE lieu_ud');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE lieu_ud (lieu_id INT NOT NULL, ud_id INT NOT NULL, INDEX IDX_8CC54E916AB213CC (lieu_id), INDEX IDX_8CC54E91DA54E4D4 (ud_id), PRIMARY KEY(lieu_id, ud_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('ALTER TABLE lieu_ud ADD CONSTRAINT FK_8CC54E916AB213CC FOREIGN KEY (lieu_id) REFERENCES lieu (id) ON UPDATE NO ACTION ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE lieu_ud ADD CONSTRAINT FK_8CC54E91DA54E4D4 FOREIGN KEY (ud_id) REFERENCES ud (id) ON UPDATE NO ACTION ON DELETE CASCADE');
+        $this->addSql('DROP TABLE user');
+    }
+}
