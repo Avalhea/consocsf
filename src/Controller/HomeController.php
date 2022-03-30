f8748d88cce4c90b005a5dc2a7056511ce016001
(J)AVA <emmaperez44@outlook.fr>
2022-03-30 11:58:42 +0200

    FAQ

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+
+    #[Route('/faq', name: 'faq')]
+    public function faq(): Response
+    {
+        return $this->render('home/faq.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+}
