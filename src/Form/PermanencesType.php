7673d7b0d7dc1b41a87b8c0d04386f171d282374
Alx <91890825+AlexBrlt@users.noreply.github.com>
2022-04-06 09:44:09 +0200

    Commit Permanence2.0 + Minor fixe

--- src/Form/PermanencesType.php
+++ src/Form/PermanencesType.php
@@ -4,8 +4,12 @@ namespace App\Form;
 
 use App\Entity\Lieu;
 use App\Entity\Permanence;
+use Doctrine\DBAL\Types\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,19 +18,39 @@ class PermanencesType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nbJours', EntityType::class,
-                ["class"=>Permanence::class])
+            ->add('nbJours',TextType::class, [
+                'label' => ' ',
+                'attr' => [
+                    'class' => 'input is-rounded', 'col-xs-2',
+                    'type' => 'int'
+        ]
+            ])
 
-            ->add('nbHeures', EntityType::class,
-                ["class"=>Permanence::class])
+            ->add('nbHeures',TextType::class, [
+                'label' => ' ',
+                'attr' => [
+                    'class' => 'input is-rounded', 'col-xs-2',
+                    'type' => 'int'
+                ]
+            ])
 
-            ->add('nbDossierSimple', EntityType::class,
-                ["class"=>Permanence::class,
-                    "choice_label"=>"name"])
 
-            ->add('nbDossierDifficile', EntityType::class,
-                ["class"=>Permanence::class,
-                    "choice_label"=>"name"]);
+
+            ->add('nbDossierSimple',TextType::class, [
+                'label' => ' ',
+                'attr' => [
+                    'class' => 'input is-rounded', 'col-xs-2',
+                    'type' => 'int'
+                ]
+            ])
+
+            ->add('nbDossierDifficile',TextType::class, [
+                'label' => ' ',
+                'attr' => [
+                    'class' => 'input is-rounded', 'col-xs-2',
+                    'type' => 'int'
+                ]
+            ]);
 
 
     }
