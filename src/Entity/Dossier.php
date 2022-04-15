94ad55224bdfa90c782b109f4df24555d0510d11
(J)AVA <emmaperez44@outlook.fr>
2022-04-15 15:09:01 +0200

    !!!

---
+++ src/Entity/Dossier.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\DossierRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: DossierRepository::class)]
+class Dossier
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    private $nbDossiers;
+
+    #[ORM\ManyToOne(targetEntity: Lieu::class, inversedBy: 'dossier')]
+    private $lieu;
+
+    #[ORM\ManyToOne(targetEntity: TypologieDossier::class, inversedBy: 'dossiers')]
+    private $typologieDossier;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNbDossiers(): ?int
+    {
+        return $this->nbDossiers;
+    }
+
+    public function setNbDossiers(?int $nbDossiers): self
+    {
+        $this->nbDossiers = $nbDossiers;
+
+        return $this;
+    }
+
+    public function getLieu(): ?Lieu
+    {
+        return $this->lieu;
+    }
+
+    public function setLieu(?Lieu $lieu): self
+    {
+        $this->lieu = $lieu;
+
+        return $this;
+    }
+
+    public function getTypologieDossier(): ?TypologieDossier
+    {
+        return $this->typologieDossier;
+    }
+
+    public function setTypologieDossier(?TypologieDossier $typologieDossier): self
+    {
+        $this->typologieDossier = $typologieDossier;
+
+        return $this;
+    }
+}
---
+++ src/Entity/TypologieDossier.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TypologieDossierRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TypologieDossierRepository::class)]
+class TypologieDossier
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 150, nullable: true)]
+    private $libelle;
+
+    #[ORM\OneToMany(mappedBy: 'typologieDossier', targetEntity: Dossier::class)]
+    private $dossiers;
+
+    public function __construct()
+    {
+        $this->dossiers = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(?string $libelle): self
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Dossier>
+     */
+    public function getDossiers(): Collection
+    {
+        return $this->dossiers;
+    }
+
+    public function addDossier(Dossier $dossier): self
+    {
+        if (!$this->dossiers->contains($dossier)) {
+            $this->dossiers[] = $dossier;
+            $dossier->setTypologieDossier($this);
+        }
+
+        return $this;
+    }
+
+    public function removeDossier(Dossier $dossier): self
+    {
+        if ($this->dossiers->removeElement($dossier)) {
+            // set the owning side to null (unless already changed)
+            if ($dossier->getTypologieDossier() === $this) {
+                $dossier->setTypologieDossier(null);
+            }
+        }
+
+        return $this;
+    }
+}
