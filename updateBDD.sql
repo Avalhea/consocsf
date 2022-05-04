46b5d5cf119a6635c2c9011941766d026b7a15d0
(J)AVA <emmaperez44@outlook.fr>
2022-05-04 11:17:16 +0200

    script updaté

--- updateBDD.sql
+++ updateBDD.sql
@@ -4,22 +4,21 @@ DROP PROCEDURE IF EXISTS delete_annuel;
 DELIMITER //
 CREATE PROCEDURE delete_annuel()
 BEGIN
-DELETE FROM action_justice;
 DELETE FROM atelier;
 DELETE FROM communication;
 DELETE FROM dossier;
 DELETE FROM dossiers_autre;
+DELETE FROM representation;
+DELETE FROM lieu;
+DELETE FROM action_justice;
 DELETE FROM evenement;
 DELETE FROM formations;
-DELETE FROM lieu;
 DELETE FROM permanence;
 END //
 
-
-
 DELIMITER //
 CREATE EVENT resetBdd
-    ON SCHEDULE EVERY 5 MINUTE
+    ON SCHEDULE EVERY 1 HOUR STARTS '2022-12-31'
     ON COMPLETION PRESERVE
     DO
 BEGIN
